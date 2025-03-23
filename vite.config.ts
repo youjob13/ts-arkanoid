e81e4afd4570ed6603c6fa101348068fca1ed375
Danil Rodionov <Danil_Rodionov@epam.com>
2025-03-23 19:44:41 +0400

    chore: add deploy

--- vite.config.ts
+++ vite.config.ts
@@ -2,22 +2,23 @@ import { defineConfig } from "vite";
 import { NodePackageImporter } from "sass-embedded";
 
 export default defineConfig({
-  root: "src",
-  appType: "spa",
-  build: {
-    cssMinify: true,
-    outDir: "../dist",
-    minify: "esbuild",
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        api: "modern",
-        importers: [new NodePackageImporter()],
-      },
+    root: "src",
+    appType: "spa",
+    base: "/ts-arkanoid",
+    build: {
+        cssMinify: true,
+        outDir: "../dist",
+        minify: "esbuild",
     },
-  },
-  dev: {
-    sourcemap: true,
-  },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                api: "modern",
+                importers: [new NodePackageImporter()],
+            },
+        },
+    },
+    dev: {
+        sourcemap: true,
+    },
 });
